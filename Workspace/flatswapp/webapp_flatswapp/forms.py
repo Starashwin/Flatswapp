9b1775443d6f96464ec521a1845a7e787485246b
Lyubomir Lazarov (student) <2072353L@student.gla.ac.uk>
2020-03-19 18:23:39 +0000

    added some models

--- Workspace/flatswapp/webapp_flatswapp/forms.py
+++ Workspace/flatswapp/webapp_flatswapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from webapp_flatswapp.models import UserProfile
+from webapp_flatswapp.models import Address
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -17,3 +18,8 @@ class UserProfileForm(forms.ModelForm):
 class Search(forms.ModelForm):
     class Meta:
         fields = ('search',)
+
+class Address(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = ('street', 'city', 'province', 'postcode',)
