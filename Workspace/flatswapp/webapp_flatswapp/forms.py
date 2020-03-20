713ada1c9167e5d8a22349081f7804ff401b235a
aliutkudemir <59883779+aliutkudemir@users.noreply.github.com>
2020-03-20 00:40:02 +0000

    Added register

--- Workspace/flatswapp/webapp_flatswapp/forms.py
+++ Workspace/flatswapp/webapp_flatswapp/forms.py
@@ -3,14 +3,20 @@ from django.contrib.auth.models import User
 from webapp_flatswapp.models import UserProfile
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'list-group-item','placeholder':'Password'}), label='')
+    username = forms.CharField(widget=forms.TextInput (attrs={'class' : 'list-group-item','placeholder':'Username'}), label='')
+    email = forms.CharField(widget=forms.TextInput (attrs={'class' : 'list-group-item','placeholder':'Email'}), label='')
     class Meta:
+    
         model = User
         fields = ('username', 'email', 'password',)
+        
 
 
 class UserProfileForm(forms.ModelForm):
-    class Meta:
+    mobile = forms.CharField(widget=forms.TextInput(attrs={'class' : 'list-group-item','placeholder':'Phone number'}), label='')
+    
+    class Meta:     
         model = UserProfile
         fields = ('mobile', 'picture',)
 
